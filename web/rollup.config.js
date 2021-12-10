e483b3b93ad2f982ab65bd0ddd83f6028a92a06c
Noah Too <krivahtoo@gmail.com>
2021-12-11 01:30:16 +0300

    Remove `sirv-cli`

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -8,27 +8,6 @@ import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-  let server;
-
-  function toExit() {
-    if (server) server.kill(0);
-  }
-
-  return {
-    writeBundle() {
-      if (server) return;
-      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-        stdio: ['ignore', 'inherit', 'inherit'],
-        shell: true
-      });
-
-      process.on('SIGTERM', toExit);
-      process.on('exit', toExit);
-    }
-  };
-}
-
 export default {
   input: 'src/main.js',
   output: {
@@ -65,10 +44,6 @@ export default {
       dedupe: ['svelte']
     }),
     commonjs(),
-
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
