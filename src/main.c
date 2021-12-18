d4f9974f6978c68163ff8e6a00c818351e1eb23f
Noah Too <krivahtoo@gmail.com>
2021-12-18 20:12:12 +0300

    Added reverse proxy for mpd stream

--- src/main.c
+++ src/main.c
@@ -32,6 +32,7 @@ static sarg_opt opts[] = {
 };
 
 static const char *s_listen_on = "ws://0.0.0.0:8000";
+static const char *s_stream_url = "http://localhost:8008";
 static const char *s_web_root = "./dist";
 
 static bool run = true;
@@ -42,7 +43,34 @@ static void sig_handler(int sig_num) {
   run = false;
 }
 
+static void forward_request(struct mg_http_message *hm, struct mg_connection *c) {
+  size_t i, max = sizeof(hm->headers) / sizeof(hm->headers[0]);
+  struct mg_str host = mg_url_host(s_stream_url);
+  mg_printf(c, "%.*s\r\n",
+            (int) (hm->proto.ptr + hm->proto.len - hm->message.ptr),
+            hm->message.ptr);
+  for (i = 0; i < max && hm->headers[i].name.len > 0; i++) {
+    struct mg_str *k = &hm->headers[i].name, *v = &hm->headers[i].value;
+    if (mg_strcmp(*k, mg_str("Host")) == 0) v = &host;
+    mg_printf(c, "%.*s: %.*s\r\n", (int) k->len, k->ptr, (int) v->len, v->ptr);
+  }
+  mg_send(c, "\r\n", 2);
+  mg_send(c, hm->body.ptr, hm->body.len);
+  LOG(LL_DEBUG, ("FORWARDING: %.*s %.*s", (int) hm->method.len, hm->method.ptr,
+                 (int) hm->uri.len, hm->uri.ptr));
+}
+
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  if (ev == MG_EV_READ && fn_data != NULL) {
+    // All incoming data from the backend, forward to the client
+    mg_send((struct mg_connection *) fn_data, c->recv.buf, c->recv.len);
+    mg_iobuf_del(&c->recv, 0, c->recv.len);
+  }
+  (void) ev_data;
+}
+
 static void server_handler(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  struct mg_connection *c2 = fn_data;
   if (ev == MG_EV_OPEN) {
     // c->is_hexdumping = 1;
   } else if (ev == MG_EV_HTTP_MSG) {
@@ -53,6 +81,19 @@ static void server_handler(struct mg_connection *c, int ev, void *ev_data, void 
       mg_ws_upgrade(c, hm, NULL);
     } else if (mg_http_match_uri(hm, "/rest")) {
       mg_http_reply(c, 200, "", "{\"result\": \"%s\"}\n", "Hello World");
+    } else if (mg_http_match_uri(hm, "/stream.mp3")) {
+      c2 = mg_connect(c->mgr, s_stream_url, fn, c);
+      if (c2 == NULL) {
+        mg_error(c, "Cannot create backend connection");
+      } else {
+        if (mg_url_is_ssl(s_stream_url)) {
+          struct mg_tls_opts opts = {.ca = "ca.pem"};
+          mg_tls_init(c2, &opts);
+        }
+        c->fn_data = c2;
+        forward_request(hm, c2);
+        // c2->is_hexdumping = 1;
+      }
     } else if (mg_http_match_uri(hm, "/*.mp3")) {
       char *delim = " ";
       char out[100];
